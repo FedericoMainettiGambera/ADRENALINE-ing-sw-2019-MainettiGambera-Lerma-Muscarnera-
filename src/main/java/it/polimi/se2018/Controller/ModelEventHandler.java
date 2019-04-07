e893591788c4aa3fe1564b836978d40dd560a73d
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2019-04-08 00:06:47 +0200

    LAB3 commits: Class Structure of Controller

---
+++ src/main/java/it/polimi/se2018/Controller/ModelEventHandler.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+public class ModelEventHandler {
+
+    public ModelEventHandler() {
+    }
+
+
+    public void update(ModelEvent event) {
+    }
+
+}
---
+++ src/main/java/it/polimi/se2018/Controller/State.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+public class State {
+
+    public State() {
+    }
+
+    public void doAction() {
+    }
+
+}
