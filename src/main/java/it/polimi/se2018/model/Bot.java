7355fdd4f434636604ca89203a206a82b794b893
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2019-04-12 14:05:00 +0200

    Bot class (model)
    Signed-off-by: Federico Mainetti Gambera <federicomainettigambera@gmail.com>

--- src/main/java/it/polimi/se2018/model/Bot.java
+++ src/main/java/it/polimi/se2018/model/Bot.java
@@ -1,16 +1,34 @@
 package it.polimi.se2018.model;
 
+/***/
 public class Bot extends Person {
 
-    
-    public Bot() {
+    /***/
+    public Bot(boolean isBotActive) {
+        super();
+        this.isBotActive = isBotActive;
     }
 
-    
+    /***/
     private boolean isBotActive;
 
-    
+    /***/
     private Player owner;
 
+    /***/
+    public Player getOwner() {
+        return owner;
+    }
+
+
+    /***/
+    public boolean isBotActive() {
+        return isBotActive;
+    }
+
+    /***/
+    public void setToNextOwner(Turns turns){
+
+    }
 
 }
