08380ab0d994fbebdc4f64f40880079c45cacce4
LudoLerma <lermaludovica@gmail.com>
2019-04-13 20:10:57 +0200

    Kill Class (model)
    Signed-off-by: LudoLerma <lermaludovica@gmail.com>

--- src/main/java/it/polimi/se2018/model/KillShotTrack.java
+++ src/main/java/it/polimi/se2018/model/KillShotTrack.java
@@ -2,16 +2,45 @@ package it.polimi.se2018.model;
 
 
 import java.util.List;
+import java.io.*;
 
 
 /***/
-public class KillShotTrack {
-
+public class KillShotTrack{
+    private List<Kill> kills;
     /***/
-    public KillShotTrack() {
+    public KillShotTrack(int number) throws Exception{
+
+
+       if(number>4)
+        {
+            if(number<8)
+
+
+            {
+                for (int index = 0; index < number; index++) {
+                    kills.add(new Kill());
+
+                }
+            }
+
+            else {
+                throw new Exception("invalid argument");
+            }
+
+
+
+        }
+
+
+
+
+
     }
 
     /***/
-    private List kills;
+
+
+
 
 }
