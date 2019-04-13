f395d9c28aa8601dd16c05cf6e7ff2e3a5c11645
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2019-04-13 20:58:26 +0200

    Board class (model)
    Signed-off-by: Federico Mainetti Gambera <federicomainettigambera@gmail.com>

--- src/main/java/it/polimi/se2018/model/Board.java
+++ src/main/java/it/polimi/se2018/model/Board.java
@@ -4,10 +4,50 @@ package it.polimi.se2018.model;
 public class Board {
 
     /***/
-    public Board(int chosenMap) {
+    public Board(String chosenMap) {
+        if(chosenMap == "map 0 Name Here"){
+            this.board=buildMap0();
+        }
+        else if(chosenMap == "map 1 Name Here"){
+            this.board=buildMap1();
+        }
+        else if(chosenMap == "map 2 Name Here"){
+            this.board=buildMap2();
+        }
+        else if(chosenMap == "map 3 Name Here"){
+            this.board=buildMap3();
+        }
     }
 
     /***/
     private Square[][] board;
+
+    /***/
+    private Square[][] buildMap0(){
+        Square[][] map = new Square[3][4];
+
+        return map;
+    }
+
+    /***/
+    private Square[][] buildMap1(){
+        Square[][] map = new Square[3][4];
+
+        return map;
+    }
+
+    /***/
+    private Square[][] buildMap2(){
+        Square[][] map = new Square[3][4];
+
+        return map;
+    }
+
+    /***/
+    private Square[][] buildMap3(){
+        Square[][] map = new Square[3][4];
+
+        return map;
+    }
 
 }
