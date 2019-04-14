e1a65402f66503e749770b2c678949fcba6c2cc6
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2019-04-14 17:58:05 +0200

    Normal class (model)
    Signed-off-by: Federico Mainetti Gambera <federicomainettigambera@gmail.com>

--- src/main/java/it/polimi/se2018/model/NormalSquare.java
+++ src/main/java/it/polimi/se2018/model/NormalSquare.java
@@ -1,14 +1,23 @@
 package it.polimi.se2018.model;
 
 
+import it.polimi.se2018.model.enumerations.SquareSide;
+import it.polimi.se2018.model.enumerations.SquareTypes;
+
 /***/
 public class NormalSquare extends Square {
 
     /***/
-    public NormalSquare() {
+    public NormalSquare(int X, int Y, SquareSide north, SquareSide east, SquareSide south, SquareSide west, SquareTypes squareType) {
+        super(X,Y,north,east,south,west,squareType);
+        this.ammoCards = null;
     }
 
     /***/
     private OrderedCardList ammoCards;
 
+    /***/
+    public OrderedCardList getAmmoCards() {
+        return ammoCards;
+    }
 }
