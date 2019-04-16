8ddab9641072ace5f3f34bf35dd5d70ed6e82477
Luca <lucamuscarnera@gmail.com>
2019-04-16 19:17:18 +0200

    Developed the possibility to load WeaponCards from file

--- src/main/java/it/polimi/se2018/model/WeaponCard.java
+++ src/main/java/it/polimi/se2018/model/WeaponCard.java
@@ -1,12 +1,16 @@
 package it.polimi.se2018.model;
 
 
+import it.polimi.se2018.model.enumerations.AmmoCubesColor;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static it.polimi.se2018.model.enumerations.AmmoCubesColor.*;
 
 
 /***/
@@ -23,7 +27,105 @@ public class WeaponCard extends Card {
     /***/
     // WeaponCard from File, polymorphic constructor
 
+    public WeaponCard(String ID) throws FileNotFoundException,IOException,InstantiationException {
+        /***
+         * It Allows To Load a card from file. In the final project there will be a folder
+         * containg a series of "Card%number%.set" where %number% is the ID of the card
+         * described by the file.
+         *
+         * The parser is quite simple.
+         * Every Line contains info about the content of the next lines or is informative content per se.
+         * It also uses the "Class" class to determinate Classes of actions.
+         *     Example Content: "cards17.set"
+         *1
+         *2          PICK UP COST
+         *3          r
+         *4          3
+         *5          PICK UP COST
+         *6          y
+         *7          1
+         *8          RELOAD COST
+         *9          b
+         *10         5
+         *11         NEW EFFECT
+         *12         ACTION
+         *13         %X%
+         *14         %Y%
+         *15         ...
+         *16         END
+         *17         NEW EFFECT
+         *18         ACTION
+         *19         %Z%
+         *20         END
+         *
+         * This File describes a card that needs 3 red cubes and 1 yellow cubes to be picked up,5 blue cubes  to reload
+         * and hass 2 effects: The first executes the atomic actions %X% and %Y%, the second executes the atomic action
+         * %Z%
+         * */
 
+        super(ID);
+
+            effects = new ArrayList<Effect>();
+            this.pickUpCost = new AmmoList();
+            this.reloadCost = new AmmoList();
+
+            BufferedReader reader = new BufferedReader(new FileReader("Card" + ID + ".set"));
+            String line;
+            line = reader.readLine();
+
+            while (line != null) {
+                    if (line.equals("PICK UP COST")) {
+                        line = reader.readLine();
+                        String secondLine = reader.readLine();
+
+                            if(line.equals("y"))
+                                pickUpCost.addAmmoCubesOfColor(yellow,Integer.parseInt(secondLine));
+                            if(line.equals("b"))
+                                pickUpCost.addAmmoCubesOfColor(blue,Integer.parseInt(secondLine));
+                            if(line.equals("r"))
+                                pickUpCost.addAmmoCubesOfColor(red,Integer.parseInt(secondLine));
+                    }
+
+                    if (line.equals("RELOAD COST")) {
+                        line = reader.readLine();
+                        String secondLine = reader.readLine();
+                        this.reloadCost = new AmmoList();
+                        if(line.equals("y"))
+                            reloadCost.addAmmoCubesOfColor(yellow,Integer.parseInt(secondLine));
+                        if(line.equals("b"))
+                            reloadCost.addAmmoCubesOfColor(blue,Integer.parseInt(secondLine));
+                        if(line.equals("r"))
+                            reloadCost.addAmmoCubesOfColor(red,Integer.parseInt(secondLine));
+                    }
+
+                    if (line.equals("NEW EFFECT"))
+                        effects.add(new Effect());
+
+                if (line.equals("ACTIONS")) {
+                    line = reader.readLine();                                    // seek the File Cursor to the next Line
+                    while (!line.equals("END")) {
+
+                        //System.out.println("AZIONE: " + line);
+                        try {
+                            Class<?> Cref = Class.forName(line);
+                            Action demo = (Action) Cref.newInstance();
+                            //System.out.println(effect.size());
+                            effects.get(effects.size() - 1).getActions().add(demo);
+                        } catch (Exception e) {
+
+                            System.out.println("errore" + e.toString());
+                        }
+
+                        line = reader.readLine();                                    //
+
+
+                    }
+
+                }
+
+                line = reader.readLine();
+            }
+        }
 
 
 
