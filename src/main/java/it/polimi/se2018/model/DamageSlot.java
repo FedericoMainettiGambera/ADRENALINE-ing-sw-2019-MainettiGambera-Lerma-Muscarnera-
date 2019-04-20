58916a4a490b172c9cef973f55d7b164433dba30
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2019-04-21 01:51:05 +0200

    DamageSlot and DamagesTracker class (model).
    Signed-off-by: Federico Mainetti Gambera <federicomainettigambera@gmail.com>

--- src/main/java/it/polimi/se2018/model/DamageSlot.java
+++ src/main/java/it/polimi/se2018/model/DamageSlot.java
@@ -1,20 +1,31 @@
 package it.polimi.se2018.model;
 
-
-/**The DamageSlot class keeps track of a single damage taken from a player.
- * THIS CLASS MUST NEVER BE USED, INSTEAD USE THE "Player" CLASS.
+/**
+ * THIS CLASS SHOULD NEVER BE DIRECTLY ACCESSED, INSTEAD USE METHODS FROM THE "Person" CLASS.
+ * The DamageSlot class keeps track of a single damage taken from a player.
+ * It represents a blood drop in the Game.
+ * @author FedericoMainettiGambera
  * */
 public class DamageSlot {
 
-    /***/
+    /*-****************************************************************************************************CONSTRUCTOR*/
+    /**Constructor:
+     * sets the shooting player.
+     * @param shootingPlayer
+     * */
     public DamageSlot(Player shootingPlayer) {
         this.shootingPlayer = shootingPlayer;
     }
 
-    /***/
+    /*-*****************************************************************************************************ATTRIBUTES*/
+    /**shooting player*/
     private Player shootingPlayer;
 
-    /***/
+    /*-********************************************************************************************************METHODS*/
+    /*Do not to use this methods directly. Instead use methods from the "Person" class.*/
+
+    /**@return
+     * */
     public Player getShootingPlayer() {
         return shootingPlayer;
     }
--- src/main/java/it/polimi/se2018/model/DamagesTracker.java
+++ src/main/java/it/polimi/se2018/model/DamagesTracker.java
@@ -3,34 +3,59 @@ package it.polimi.se2018.model;
 import java.util.ArrayList;
 import java.util.List;
 
-/**The DamagesTracker class keeps track of the damages taken from a player.
- * THIS CLASS MUST NEVER BE USED, INSTEAD USE THE "Player" CLASS.
+/**
+ * THIS CLASS SHOULD NEVER BE DIRECTLY ACCESSED, INSTEAD USE METHODS FROM THE "Person" CLASS.
+ * The DamagesTracker class keeps track of the damages taken from a player.
+ * @author FedericoMainettiGambera
  * */
 public class DamagesTracker {
 
-    /***/
+    /*-****************************************************************************************************CONSTRUCTOR*/
+    /**Constructor:
+     * initialize the damageSlotsList with a new Arraylist of DamageSlot
+     * */
     public DamagesTracker() {
         this.damageSlotsList = new ArrayList<>();
     }
 
-    /***/
+    /*-*****************************************************************************************************ATTRIBUTES*/
+    /**damage slots list*/
     private List<DamageSlot> damageSlotsList;
 
-    /***/
+    /*-********************************************************************************************************METHODS*/
+    /*Do not to use this methods directly. Instead use methods from the "Person" class.*/
+
+    /**avoid this method if possible, do not access directly attributes, but use method that interact with them for you.
+     * @return
+     * */
+    public List<DamageSlot> getDamageSlotsList() {
+        return damageSlotsList;
+    }
+
+    /**add Damages
+     * @param shootingPlayer
+     * @param numberOfDamages
+     * */
     public void addDamages(Player shootingPlayer, int numberOfDamages) {
         for(int i = 0; i<numberOfDamages; i++) {
             damageSlotsList.add(new DamageSlot(shootingPlayer));
         }
     }
 
-    /***/
+    /**@param slotNumber
+     * @return the slotNumber DamageSlot
+     * */
     public DamageSlot getDamageSlot(int slotNumber) {
-        return damageSlotsList.get(slotNumber);
+        if(damageSlotsList.size()>= slotNumber) {
+            return damageSlotsList.get(slotNumber);
+        }
+        return null;
     }
 
-    /***/
+    /**empty the damageSlotsList
+     * (used if a player dies)
+     * */
     public void emptyList() {
         this.damageSlotsList = new ArrayList<>();
     }
-
 }
