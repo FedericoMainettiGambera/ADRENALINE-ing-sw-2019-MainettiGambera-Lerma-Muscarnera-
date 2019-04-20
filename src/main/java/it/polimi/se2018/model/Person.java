a6218fc5598994b8999cd61c60533f439ada7191
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2019-04-21 01:52:31 +0200

    PlayerBoard class and all methods related in Person (model).
    Signed-off-by: Federico Mainetti Gambera <federicomainettigambera@gmail.com>

--- src/main/java/it/polimi/se2018/model/Person.java
+++ src/main/java/it/polimi/se2018/model/Person.java
@@ -3,11 +3,19 @@ package it.polimi.se2018.model;
 import it.polimi.se2018.model.enumerations.AmmoCubesColor;
 import it.polimi.se2018.model.enumerations.PlayersColors;
 
-
-/***/
+/**Abstract class that represents a character.
+ * There are all the methods needed to do any action required during the game.
+ * @author FedericoMainettiGambera
+ * */
 public abstract class Person {
 
-    /***/
+    /*-****************************************************************************************************CONSTRUCTOR*/
+    /**Constructor
+     * Initialize nickname and color with the parameters, create a new board, set the score to zero
+     * and position to null.
+     * @param color
+     * @param nickname
+     * */
     public Person(String nickname, PlayersColors color){
         this.nickname=nickname;
         this.color=color;
@@ -17,28 +25,28 @@ public abstract class Person {
         this.position=null;
     }
 
-
-    /*ATTRIBUTES*/
-
-    /***/
+    /*-*****************************************************************************************************ATTRIBUTES*/
+    /**nickname*/
     private String nickname;
 
-    /***/
+    /**color*/
     private PlayersColors color;
 
-    /***/
+    /**current player score*/
     private int score;
 
-    /***/
+    /**
+     * Current player position.
+     * Null only if the game hasn't already started
+     * */
     private Position position;
 
-    /***/
+    /**player board*/
     private PlayerBoard board;
 
+    /*-********************************************************************************************************METHODS*/
 
-    /*METHODS*/
-
-                /*position*/
+    /*POSITION*/
     /***/
     public void setPosition(int x, int y){
         this.position = new Position(x,y);
@@ -49,73 +57,186 @@ public abstract class Person {
         return position;
     }
 
-                /*points*/
+    /*POINTS*/
     /***/
     public void AddPoints(int points) {
         this.score+=points;
     }
 
-                /*nickname*/
-    /***/
-    public String getNickname(){
-      return nickname;
-    }
-
-                /*color*/
-    /***/
-    public PlayersColors getColor(){
-        return color;
-    }
-
-                /*color*/
     /***/
     public int getScore(){
         return score;
     }
 
-                /*PlayerBoard*/
-    /** USELESS (?) -> probably YES*/
-    public PlayerBoard getPlayerBoard() {
-        return board;
-    }
+    /*NICKNAME*/
     /***/
+    public String getNickname(){
+      return nickname;
+    }
+
+    /*COLOR*/
+    /***/
+    public PlayersColors getColor(){
+        return color;
+    }
+
+    /*PLAYER BOARD*/
+    /**returns the player board.
+     * This method should be used as less as possible, instead search for the right method.
+     * @return
+     * */
+    public PlayerBoard getPlayerBoard() { return board; }
+
+    /**increment the death counter by one*/
     public void addDeath() {
         this.board.addDeath();
     }
-    /***/
+
+    /**return the number of times the player has died.
+     * @return
+     * */
     public int getDeathCounter() {
         return this.board.getDeathCounter();
     }
-    /***/
+
+    /**@return returns an array of integer that represents the amount of points the first (second, third, etc..) player
+     * with more hits will get. (es: 8,6,4,2,1,1,1,1,1,1)
+     * */
+    public int[] getPointsList(){
+        int[] pointsList = new int[10];
+        for(int i = 0; i < 10; i++){
+            pointsList[i]= Math.max( (8-(2*i)-(this.getDeathCounter()*2)) , 1);
+        }
+        return pointsList;
+    }
+
+    /**adding ammo to the player ammo box
+     * @param color
+     * @param quantity
+     * */
     public void addAmmoCubes(AmmoCubesColor color, int quantity) {
         this.board.addAmmoCubes(color, quantity);
     }
-    /***/
+
+    /**adding ammo to the player ammo box
+     * @param ammoList
+     */
+    public void addAmmoCubes(AmmoList ammoList){
+        this.board.addAmmoCubes(ammoList);
+    }
+
+    /**subtract a specific amount of ammos.
+     * Before doing any operation checks if it is possible to subtract the specified amount.
+     * @param color
+     * @param quantity
+     * @return
+     * */
     public boolean payAmmoCubes(AmmoCubesColor color, int quantity){
         return this.board.payAmmoCubes(color, quantity);
     }
-    /***/
+    /**subtract a specific amount of ammos
+     * Before doing any operation checks if it is possible to subtract the specified amount.
+     * @param cost
+     * @return
+     * */
     public boolean payAmmoCubes(AmmoList cost){
         return this.board.payAmmoCubes(cost);
     }
-    /***/
+    /**checks if it is possible to subtract a specific amount of ammos
+     * @param color
+     * @param quantity
+     * @return
+     * */
     public boolean canPayAmmoCubes(AmmoCubesColor color, int quantity){ return this.board.canPayAmmoCubes(color,quantity); }
-    /***/
+
+    /**checks if it is possible to subtract a specific amount of ammos
+     * @param cost
+     * @return
+     * */
     public boolean canPayAmmoCubes(AmmoList cost){ return this.board.canPayAmmoCubes(cost); }
-    /***/
+
+    /** add a specific amount of blood drops (damages) of the shootingPlayer to the (this) player board.
+     * if adding damages overkills the this player, it marks the shootingPlayer.
+     * @param numberOfDamages
+     * @param shootingPlayer
+     * */
     public void addDamages (Player shootingPlayer, int numberOfDamages){
         this.board.addDamages(shootingPlayer,numberOfDamages);
+        if(isOverkilled()){
+            shootingPlayer.addMarksFrom( (Player)this, 1);
+        }
     }
-    /***/
+
+    /** takes away all the damages from the player board.
+     * (this method will be used when the player dies)*/
     public void emptyDamagesTracker(){
         this.board.emptyDamagesTracker();
     }
-    /***/
+
+    /**checks if the player has received at least 11 damages (is dead)
+     * @return */
+    public boolean isDead(){
+        if(this.board.getDamagesTracker().getDamageSlotsList().size()>10){
+            return true;
+        }
+        return false;
+    }
+
+    /**checks if the player has received at least 12 damages (is overKilled)
+     * @return */
+    public boolean isOverkilled(){
+        if(this.board.getDamagesTracker().getDamageSlotsList().size()>11){
+            return true;
+        }
+        return false;
+    }
+
+    /**checks if the player has received at least 3 damages
+     * @return */
+    public boolean hasAdrenalineGrabAction(){
+        if(this.board.getDamagesTracker().getDamageSlotsList().size()>2){
+            return true;
+        }
+        return false;
+    }
+
+    /**checks if the player has received at least 6
+     * @return */
+    public boolean hasAdrenalineShootAction(){
+        if(this.board.getDamagesTracker().getDamageSlotsList().size()>5){
+            return true;
+        }
+        return false;
+    }
+
+    /**add a specific amount of marks from the markingPlayer to (this) player board
+     * @param markingPlayer
+     * @param quantity
+     * */
     public void addMarksFrom(Player markingPlayer, int quantity){
         this.board.addMarksFrom(markingPlayer,quantity);
     }
-    /***/
-    public void getMarksFrom(Player markingPlayer){
-        this.board.getMarksFrom(markingPlayer);
+
+    /**return the number of marks the player has received from the markingPlayer
+     * @param markingPlayer
+     * @return
+     * */
+    public int getMarksFrom(Player markingPlayer){
+        return this.board.getMarksFrom(markingPlayer);
+    }
+
+    /**delete all the marks received from the markingPlayer
+     * @param markingPlayer
+     * */
+    public void deleteMarksFromPlayer(Player markingPlayer){
+        this.board.deleteMarksFromPlayer(markingPlayer);
+    }
+
+    /**takes all the marks from a player and tranform them in dadmages
+     * @param shootingPlayer
+     * */
+    public void applyMarksFromPlayer(Player shootingPlayer){
+        addDamages(shootingPlayer, this.getMarksFrom(shootingPlayer));
+        deleteMarksFromPlayer(shootingPlayer);
     }
 }
--- src/main/java/it/polimi/se2018/model/PlayerBoard.java
+++ src/main/java/it/polimi/se2018/model/PlayerBoard.java
@@ -3,16 +3,16 @@ package it.polimi.se2018.model;
 import it.polimi.se2018.model.enumerations.AmmoCubesColor;
 
 /**
- * The PlayerBoard class represents the player's board and keeps track of the current ammunitions, the current damage
- * taken and the number of marks.
- * THIS CLASS MUST NEVER BE USED, INSTEAD USE THE "Player" CLASS.
+ * THIS CLASS SHOULD NEVER BE DIRECTLY ACCESSED, INSTEAD USE METHODS FROM THE "Person" CLASS.
+ * The PlayerBoard class represents a player's board and keeps track of the current ammunitions, the current damage
+ * taken and the number of marks and deaths.
+ * @author FedericoMainettiGambera
  * */
 public class PlayerBoard {
 
-    /*CONSTRUCTOR*/
-
-    /**
-     * Initialize the the AmmoBox with "GameConstant.NumberOfStartingAmmos" ammos for each color.
+    /*-****************************************************************************************************CONSTRUCTOR*/
+    /**Constructor:
+     * Initialize the AmmoBox with "GameConstant.NumberOfStartingAmmos" ammos for each color.
      * The damagesTracker and marksTracker are two empty ArrayList.
      * The deathCounter is set to zero.
      * */
@@ -28,101 +28,147 @@ public class PlayerBoard {
         this.deathCounter = 0;
     }
 
-
-
-    /*ATTRIBUTES*/
-
-    /***/
+    /*-*****************************************************************************************************ATTRIBUTES*/
+    /**ammo box*/
     private AmmoList ammoBox;
 
-    /***/
+    /**damages tracker*/
     private DamagesTracker damagesTracker;
 
-    /***/
+    /**marks tracker*/
     private MarksTracker marksTracker;
 
-    /***/
+    /**number of times the player has died*/
     private int deathCounter;
 
 
 
-    /*METHODS*/
+    /*-********************************************************************************************************METHODS*/
+    /*Do not to use this methods directly. Instead use methods from the "Person" class.*/
 
-                /*DeathCounter*/
-
-    /***/
+    /*DEATH COUNTER*/
+    /**increment the deathCounter by one*/
     public void addDeath() {
         deathCounter++;
     }
 
-    /***/
+    /**@return number of times the player has died
+     * */
     public int getDeathCounter() {
         return deathCounter;
     }
 
-                /*AmmoBox*/
-
-    /** USELESS (?) -> probably YES*/
+    /*AMMO BOX*/
+    /**avoid this method if possible, do not access directly attributes, but use method that interact with them for you.
+     * @return
+     * */
     public AmmoList getAmmoBox() {
         return ammoBox;
     }
 
-    /***/
+    /**add ammo cubes
+     * @param color
+     * @param quantity
+     * */
     public void addAmmoCubes(AmmoCubesColor color, int quantity) {
         this.ammoBox.addAmmoCubesOfColor(color, quantity);
     }
+    /**add ammo cubes
+     * @param ammoList
+     * */
+    public void addAmmoCubes(AmmoList ammoList) {
+        for(int i = 0; i < ammoList.getAmmoCubesList().size(); i++){
+            this.ammoBox.addAmmoCubesOfColor( ammoList.getAmmoCubesList().get(i).getColor(),
+                                              ammoList.getAmmoCubesList().get(i).getQuantity() );
+        }
+    }
 
-    /***/
+    /**pay ammo cubes
+     * @param quantity
+     * @param color
+     * @return */
     public boolean payAmmoCubes(AmmoCubesColor color, int quantity){
         return this.ammoBox.payAmmoCubes(color, quantity);
     }
-
-    /***/
+    /**pay ammo cubes
+     * @param cost
+     * @return */
     public boolean payAmmoCubes(AmmoList cost){
         return this.ammoBox.payAmmoCubes(cost);
     }
 
-    /***/
+    /**checks if it is possible to pay an amount of ammo
+     * @param color
+     * @param quantity
+     * @return
+     * */
     public boolean canPayAmmoCubes(AmmoCubesColor color, int quantity){
         return this.ammoBox.canPayAmmoCubes(color,quantity);
     }
-
-    /***/
+    /**checks if it is possible to pay an amount of ammo
+     * @param cost
+     * @return
+     * */
     public boolean canPayAmmoCubes(AmmoList cost){
         return this.ammoBox.canPayAmmoCubes(cost);
     }
 
-    /*DamagesTracker*/
+    /*DAMAGES TRACKER*/
+    /**avoid this method if possible, do not access directly attributes, but use method that interact with them for you.
+     * @return
+     * */
+    public DamagesTracker getDamagesTracker(){
+        return this.damagesTracker;
+    }
 
-    /** USELESS (?) -> probably YES*/
+    /**return a specific slot from the DamageSlot
+     * @param slotNumber
+     * @return null if the specified slotNumber doesn't exists.
+     * @return the specified slot.
+     * */
     public DamageSlot getDamagesSlot(int slotNumber) {
         return damagesTracker.getDamageSlot(slotNumber);
     }
 
-    /***/
+    /**add damages
+     * @param shootingPlayer
+     * @param numberOfDamages
+     * */
     public void addDamages (Player shootingPlayer, int numberOfDamages){
         this.damagesTracker.addDamages(shootingPlayer,numberOfDamages);
     }
 
-    /***/
+    /**empty the damages Tracker*/
     public void emptyDamagesTracker(){
         this.damagesTracker.emptyList();
     }
 
-                /*MarksTraker*/
-
-    /** USELESS (?) -> probably YES*/
+    /*MARKS TRACKER*/
+    /**avoid this method if possible, do not access directly attributes, but use method that interact with them for you.
+     * @return
+     * */
     public MarksTracker getMarksTracker() {
         return marksTracker;
     }
 
-    /***/
+    /**add Marks
+     * @param quantity
+     * @param markingPlayer
+     * */
     public void addMarksFrom(Player markingPlayer, int quantity){
         this.marksTracker.addMarksFrom(markingPlayer,quantity);
     }
 
-    /***/
-    public void getMarksFrom(Player markingPlayer){
-        this.marksTracker.getNumberOfMarksSlotFrom(markingPlayer);
+    /**@param markingPlayer
+     * @return the number of marks received from the specified player*/
+    public int getMarksFrom(Player markingPlayer){
+        return this.marksTracker.getNumberOfMarksSlotFrom(markingPlayer);
+    }
+
+    /**delete all the marks received from the markingPlayer
+     * @param markingPlayer
+     * */
+    public void deleteMarksFromPlayer(Player markingPlayer){
+        this.marksTracker.deleteMarksFromPlayer(markingPlayer);
     }
 }
