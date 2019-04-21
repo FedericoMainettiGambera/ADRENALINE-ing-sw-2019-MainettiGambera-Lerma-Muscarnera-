86330054314abb9a34e3f5bea5321deccef288b8
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2019-04-21 05:41:53 +0200

    Bot class (model)
    Signed-off-by: Federico Mainetti Gambera <federicomainettigambera@gmail.com>

--- src/main/java/it/polimi/se2018/model/Bot.java
+++ src/main/java/it/polimi/se2018/model/Bot.java
@@ -5,32 +5,43 @@ import it.polimi.se2018.model.enumerations.PlayersColors;
 /***/
 public class Bot extends Person {
 
-    /***/
+    /*-****************************************************************************************************CONSTRUCTOR*/
+    /**Constructor:
+     * uses the Person constructor, also set isBotActive and set owner to null
+     * @param color
+     * @param nickname
+     * @param isBotActive
+     * */
     public Bot(boolean isBotActive, String nickname, PlayersColors color) {
         super(nickname, color);
         this.isBotActive = isBotActive;
+        this.owner = null;
     }
 
-    /***/
+    /*-*****************************************************************************************************ATTRIBUTES*/
+    /**is bot active*/
     private boolean isBotActive;
 
-    /***/
+    /**owner*/
     private Player owner;
 
-
-    /***/
+    /*-********************************************************************************************************METHODS*/
+    /**@return
+     * */
     public Player getOwner() {
         return owner;
     }
 
-    /***/
+    /**sets the bot owner to the next playing Player
+     * @param turns
+     * */
+    public void setToNextOwner(Turns turns){
+        /*TODO*/
+    }
+
+    /**@return isBotActive
+     * */
     public boolean isBotActive() {
         return isBotActive;
     }
-
-    /***/
-    public void setToNextOwner(Turns turns){
-
-    }
-
 }
