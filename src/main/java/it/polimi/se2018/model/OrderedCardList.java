ea33f55846c5369e33845b1fc8ee575e9f9dc7c2
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2019-04-21 20:31:10 +0200

    OrderedCardList class (model)
    Signed-off-by: Federico Mainetti Gambera <federicomainettigambera@gmail.com>

--- src/main/java/it/polimi/se2018/model/OrderedCardList.java
+++ src/main/java/it/polimi/se2018/model/OrderedCardList.java
@@ -1,20 +1,25 @@
 package it.polimi.se2018.model;
 
-
 import java.util.List;
 
-
-/***/
+/**this class is an ordered card list of a specified type of cards, it is used to represents WeaponCards and
+ * PowerUpCards in game decks or players hands.
+ * */
 public class OrderedCardList<Card> {
 
-    /***/
+    /*-****************************************************************************************************CONSTRUCTOR*/
+    /**Constructor:
+     * */
     public OrderedCardList() {
     }
 
-    /***/
+    /*-*****************************************************************************************************ATTRIBUTES*/
+    /**cards*/
     private List<Card> cards;
 
-    /***/
+    /*-********************************************************************************************************METHODS*/
+    /**@return
+     * */
     public List<Card> getCards() {
         return cards;
     }
