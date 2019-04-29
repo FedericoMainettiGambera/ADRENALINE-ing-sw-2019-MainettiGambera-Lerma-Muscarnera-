f3596e4512f52e99eff78619e568baecc28673b3
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2019-04-29 17:21:27 +0200

    Effect class (model)
    Signed-off-by: Federico Mainetti Gambera <federicomainettigambera@gmail.com>

--- src/main/java/it/polimi/se2018/model/events/ModelViewEvent.java
+++ src/main/java/it/polimi/se2018/model/events/ModelViewEvent.java
@@ -1,6 +1,6 @@
 package it.polimi.se2018.model.events;
 
-/**this class holds all the information needed to update the View when the Model changes*/
+/**this class holds all the information needed to update the view when the Model changes*/
 public class ModelViewEvent extends Event {
 
     /*-****************************************************************************************************CONSTRUCTOR*/
