b257c580639788b3845408963d7d60b6b7a65296
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2019-04-29 02:43:58 +0200

    revert events
    Signed-off-by: Federico Mainetti Gambera <federicomainettigambera@gmail.com>

--- src/main/java/it/polimi/se2018/model/events/ModelViewEvent.java
+++ src/main/java/it/polimi/se2018/model/events/ModelViewEvent.java
@@ -4,35 +4,9 @@ package it.polimi.se2018.model.events;
 public class ModelViewEvent extends Event {
 
     /*-****************************************************************************************************CONSTRUCTOR*/
-    /**Constructor
-     * */
-    public ModelViewEvent(ModelViewEventType component){
-        this.component = component;
-    }
 
     /*-*****************************************************************************************************ATTRIBUTES*/
-    /**contains information about wich component of the model generated the Event*/
-    private ModelViewEventType component;
 
     /*-********************************************************************************************************METHODS*/
 
-    /**@return
-     */
-    public ModelViewEventType getComponent() {
-        return component;
-    }
-
-    /**@return all the information that contains but in a String version
-     */
-    @Override
-    public String stringify(){
-        return super.toString();
-    }
-
-    /**builds the event from a String ( inverse of toString())
-     * */
-    @Override
-    public void parse(String informations){
-        super.parse(informations);
-    }
 }
--- src/main/java/it/polimi/se2018/model/events/ViewControllerEvent.java
+++ src/main/java/it/polimi/se2018/model/events/ViewControllerEvent.java
@@ -4,25 +4,9 @@ package it.polimi.se2018.model.events;
 public class ViewControllerEvent extends Event {
 
     /*-****************************************************************************************************CONSTRUCTOR*/
-    /**Constructor
-     * */
-    public ViewControllerEvent(){
-    }
 
     /*-*****************************************************************************************************ATTRIBUTES*/
 
     /*-********************************************************************************************************METHODS*/
-    /**@return all the information that contains but in a String version
-     */
-    @Override
-    public String stringify(){
-        return super.toString();
-    }
 
-    /**builds the event from a String ( inverse of toString())
-     * */
-    @Override
-    public void parse(String informations){
-        super.parse(informations);
-    }
 }
