3f22ed3bb96d23bef31415885807674a700df183
LudoLerma <lermaludovica@gmail.com>
2019-05-03 19:33:09 +0200

    gamesetup
    Signed-off-by: LudoLerma <lermaludovica@gmail.com>

--- src/main/java/it/polimi/se2018/model/GameSetUp.java
+++ src/main/java/it/polimi/se2018/model/GameSetUp.java
@@ -1,13 +1,21 @@
 package it.polimi.se2018.model;
 
 import it.polimi.se2018.model.enumerations.GameMode;
+import java.lang.*;
 
 
 /***/
 public class GameSetUp {
 
     /***/
-    public GameSetUp() {
+    public GameSetUp(){
+        this.gameMode=null;
+        this.numberOfStartingSkulls=0;
+        this.isDoubleKill=false;
+        this.isFinalFrenzy=false;
+        this.mapChoice=null;
+
+
     }
 
     /***/
@@ -25,7 +33,44 @@ public class GameSetUp {
     /***/
     private int numberOfStartingSkulls;
 
-    /***/
-    private Game game;
+
+
+    /** setters  */
+    public void setGameMode(GameMode gameMode){
+
+        this.gameMode=gameMode;
+    }
+
+    public void setMapChoice(String chosenMap){
+
+        this.mapChoice=chosenMap;
+
+    }
+
+    public void setDoubleKill(boolean isDoubleKill){
+
+        this.isDoubleKill=isDoubleKill;
+
+    }
+
+    public void setNumberOfStartingSkulls(int numberOfStartingSkulls){
+
+        this.numberOfStartingSkulls=numberOfStartingSkulls;;
+
+    }
+
+    public void setFinalFrenzy(boolean finalFrenzy) {
+        isFinalFrenzy = finalFrenzy;
+    }
+
+    /**getters*/
+
+    public GameMode getGameMode(){return this.gameMode; }
+    public  String getMapChoice(){ return this.mapChoice;}
+    public boolean getIsDoubleKill(){ return this.isDoubleKill;}
+    public int getNumberOfStartingSkulls(){ return this.numberOfStartingSkulls;}
+    public boolean getIsFinalFrenzy(){ return this.isFinalFrenzy; }
+
+
 
 }
