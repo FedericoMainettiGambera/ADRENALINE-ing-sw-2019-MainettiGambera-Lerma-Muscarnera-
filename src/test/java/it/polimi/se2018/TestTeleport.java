7599d2b6f12580c5a7ff55ac14d07e958da0ed6e
Luca <lucamuscarnera@gmail.com>
2019-05-05 23:47:05 +0200

    - 100% testTelport - 100% testTurretSquare

--- src/test/java/it/polimi/se2018/TestTeleport.java
+++ src/test/java/it/polimi/se2018/TestTeleport.java
@@ -1,9 +1,6 @@
 package it.polimi.se2018;
 
-import it.polimi.se2018.model.Action;
-import it.polimi.se2018.model.Player;
-import it.polimi.se2018.model.Position;
-import it.polimi.se2018.model.Teleport;
+import it.polimi.se2018.model.*;
 import org.junit.Test;
 
 public class TestTeleport {
@@ -15,5 +12,19 @@ public class TestTeleport {
         Position newPosition = new Position(5,5);
         A.getActionInfo().getActionDetails().getUserSelectedActionDetails().setNewPosition(newPosition);
         A.Exec();
+    }
+    @Test
+    public void testTeleportSecondConstructor() {
+        Teleport T = new Teleport(new ActionInfo()) ;
+    }
+    @Test
+    public void testsetDefaultSetting() {
+        Teleport T = new Teleport();
+        T.setDefaultSetting();
+    }
+    @Test
+    public void testupdateSettingsFromFile() {
+        Teleport T = new Teleport();
+        T.updateSettingsFromFile();
     }
 }
--- src/test/java/it/polimi/se2018/TestTurretSquare.java
+++ src/test/java/it/polimi/se2018/TestTurretSquare.java
@@ -1,9 +1,12 @@
 package it.polimi.se2018;
 
+import it.polimi.se2018.model.Player;
 import it.polimi.se2018.model.TurretSquare;
 import it.polimi.se2018.model.enumerations.SquareSide;
 import it.polimi.se2018.model.enumerations.SquareTypes;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class TestTurretSquare {
@@ -16,5 +19,19 @@ public class TestTurretSquare {
         SquareSide S = SquareSide.wall;
         SquareSide O = SquareSide.wall;
         TurretSquare T = new TurretSquare(X,Y,N,E,S,O, SquareTypes.spawnPoint,'r');
+    }
+    @Test
+    public void testGetOwner() {
+        int X = 5;
+        int Y = 5;
+        SquareSide N = SquareSide.wall;
+        SquareSide E = SquareSide.wall;
+        SquareSide S = SquareSide.wall;
+        SquareSide O = SquareSide.wall;
+        TurretSquare T = new TurretSquare(X,Y,N,E,S,O, SquareTypes.spawnPoint,'r');
+        Player p = new Player();
+        T.setOwner(p);
+        assertEquals(T.getOwner(),p);
+
     }
 }
