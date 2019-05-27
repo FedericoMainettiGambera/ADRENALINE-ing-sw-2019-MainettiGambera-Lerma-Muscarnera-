419b30f7b30350884597926c4f68d1e18bb3e936
Luca <lucamuscarnera@gmail.com>
2019-05-27 22:03:59 +0200

    Rewrote weaponcard constructor

--- src/test/java/it/polimi/se2019/TestWeaponCard.java
+++ src/test/java/it/polimi/se2019/TestWeaponCard.java
@@ -1,8 +1,15 @@
 package it.polimi.se2019;
 
+import it.polimi.se2019.model.Action;
+import it.polimi.se2019.model.Effect;
 import it.polimi.se2019.model.WeaponCard;
 import it.polimi.se2019.model.enumerations.EffectInfoType;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestWeaponCard {
     @Test
@@ -23,6 +30,57 @@ public class TestWeaponCard {
         } catch (Exception e) {
                 System.out.println("errore : " + e.toString());
 
+
+        }
+    }
+    @Test
+    public void testAlpha() throws Exception{
+        int caso = 2;
+        String ID = "Test";
+        if(caso == 1) {
+            testerA(ID);
+        }
+        else {
+            WeaponCard weaponCard = new WeaponCard(ID);
+            System.out.println(weaponCard.getEffects().size());
+
+        }
+    }
+    private void testerA(String ID) throws Exception{
+        BufferedReader reader = new BufferedReader(new FileReader("src/main/Files/cards/weaponCards/card"+ID+".set"));
+        List<Effect> effects = new ArrayList<>();
+
+        try {
+            String line;
+            line = reader.readLine();
+
+            while (line != null) {
+                if (line.equals("NEW EFFECT"))
+                    effects.add(new Effect());
+
+                System.out.println(line);
+                line = reader.readLine();
+            }
+        } catch(Exception e) {;}
+    System.out.println("---");
+    System.out.println(effects.size());
+    }
+    @Test
+    public void testCard9() {
+        try {
+        WeaponCard weaponCard = new WeaponCard("Test");
+        System.out.println(weaponCard.getEffects().size());
+        for(Effect e:weaponCard.getEffects() ) {
+            System.out.println(e.toString());
+            for(Action a: e.getActions()) {
+
+                System.out.println(a.toString());
+            }
+
+
+        }
+        } catch(Exception e) {
+            System.out.println("##" + e.toString());
 
         }
     }
