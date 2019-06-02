33f92d80ba0efd7ff0809a30e250e542056ed17f
LudoLerma <lermaludovica@gmail.com>
2019-06-02 20:40:15 +0200

    GUI
    Signed-off-by: LudoLerma <lermaludovica@gmail.com>

--- src/main/java/it/polimi/se2019/view/outputHandler/GUIInizialScene.java
+++ src/main/java/it/polimi/se2019/view/outputHandler/GUIInizialScene.java
@@ -1,9 +1,11 @@
 package it.polimi.se2019.view.outputHandler;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -16,7 +18,7 @@ import javax.swing.text.LabelView;
 import javax.swing.text.Position;
 import java.io.FileInputStream;
 
-public class GUIInizialScene extends Application{
+public class GUIInizialScene extends Application implements EventHandler<ActionEvent>{
 
 
 
@@ -28,8 +30,7 @@ public class GUIInizialScene extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        StackPane pane=new StackPane();
-        StackPane pane1=new StackPane();
+
         Image image=new Image(new FileInputStream("src/main/Files/Images/InizialImage/Adrenaline.png"));
         ImageView imageView=new ImageView(image);
 
@@ -39,7 +40,7 @@ public class GUIInizialScene extends Application{
         Image image2=new Image(new FileInputStream("src/main/Files/Images/InizialImage/Settings.png"));
         ImageView imageView2=new ImageView(image2);
 
-
+        StackPane pane=new StackPane();
         Scene scene=new Scene(pane, 500, 500);
         imageView.fitWidthProperty().bind(scene.widthProperty());
         imageView.fitHeightProperty().bind(scene.heightProperty());
@@ -50,30 +51,18 @@ public class GUIInizialScene extends Application{
         imageView2.setFitHeight(25);
         imageView1.setSmooth(true);
 
+
+        StackPane pane1=new StackPane();
+        VBox vbox=new VBox();
+
+        vbox.getChildren().addAll(imageView1,imageView2);
+
         pane.getChildren().add(imageView);
-        pane.getChildren().add(imageView1);
-        pane1.getChildren().add(imageView2);
-
-        imageView.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event){
-
-                GUIOutputHandler gui=new GUIOutputHandler();
-
-                try {
-                    gui.start(primaryStage);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                    launch();
+       // pane.getChildren().add(imageView1);
+        pane1.getChildren().addAll(vbox);
 
 
-
-            }
-        });
-
-        pane.setAlignment(Pos.CENTER_LEFT);
+       // pane.setAlignment(Pos.CENTER_LEFT);
 
 
         pane.getChildren().add(pane1);
@@ -86,4 +75,8 @@ public class GUIInizialScene extends Application{
     }
 
 
+    @Override
+    public void handle(ActionEvent event) {
+
+    }
 }
