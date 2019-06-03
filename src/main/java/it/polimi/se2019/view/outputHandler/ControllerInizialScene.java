0577f49772befe86ca9a5f8fc8298710ea5de6af
LudoLerma <lermaludovica@gmail.com>
2019-06-03 17:21:31 +0200

    GUI
    Signed-off-by: LudoLerma <lermaludovica@gmail.com>

---
+++ src/main/java/it/polimi/se2019/view/outputHandler/ControllerInizialScene.java
@@ -0,0 +1,29 @@
+package it.polimi.se2019.view.outputHandler;
+
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerInizialScene implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("View is now loaded!");
+    }
+
+    public void fitProperties(){
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
