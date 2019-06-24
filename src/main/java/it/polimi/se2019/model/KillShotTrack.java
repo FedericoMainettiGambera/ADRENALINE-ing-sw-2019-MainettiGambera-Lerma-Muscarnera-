99b845e633080f8da8323e971ce83018f42a7304
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2019-06-24 02:08:37 +0200

    TODO
    Signed-off-by: Federico Mainetti Gambera <federicomainettigambera@gmail.com>

--- src/main/java/it/polimi/se2019/model/KillShotTrack.java
+++ src/main/java/it/polimi/se2019/model/KillShotTrack.java
@@ -64,8 +64,8 @@ public class KillShotTrack extends Observable implements Serializable {
      * @param killingPlayer @tracks down whom committed a kill
      * */
     public void deathOfPlayer(Player killingPlayer, boolean isOverKill){
-        
-        //TODO:
+
+        //TODO: read following comment
         //prendiamo per ipotesi un gioco a 5 startingSkulls e quindi una killshot track a 5 Kills
         //durante il gioco muoiono le 5 persone e viene triggerata la FF
         //se nella FF muore qualcuno quest'ultimo non viene inserito nella killshot track ! problema !
