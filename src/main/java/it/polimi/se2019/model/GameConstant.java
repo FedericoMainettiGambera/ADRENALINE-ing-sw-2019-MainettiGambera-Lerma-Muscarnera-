853a54b536bf6b80e8b480d5153e6ee44b26e18b
Federico Mainetti Gambera <federicomainettigambera@gmail.com>
2019-07-05 04:07:41 +0200

    fixing timer inputs
    Signed-off-by: Federico Mainetti Gambera <federicomainettigambera@gmail.com>

--- src/main/java/it/polimi/se2019/model/GameConstant.java
+++ src/main/java/it/polimi/se2019/model/GameConstant.java
@@ -20,7 +20,7 @@ public class GameConstant implements Serializable {
     public static final int COUNTDOWN_IN_SECONDS_FOR_CONNECTION_QUEUE = 23;
 
     /**time for the user to insert an input before they are set AFK*/
-    public static final int TIME_TO_INSERT_INPUT_IN_SECONDS = 5;
+    public static final int TIME_TO_INSERT_INPUT_IN_SECONDS = 100;
 
     /**maximum number of skulls tha can be placed  on the kill shot track*/
     public static final int MAX_STARTING_SKULLS = 8;
